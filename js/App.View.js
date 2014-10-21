e45db7d6bc90fdda9d242d244ded857459f38237
Evan MacGregor <macgregor.evan@gmail.com>
2014-10-21 15:47:44 -0400

    Revert "I hate git so much it hurts"
    This reverts commit 0b97669fb010ad610b5bfd0b4f9e427c16206ef9.

---
+++ js/App.View.js
@@ -0,0 +1,29 @@
+var appView = Backbone.View.extend({
+	el: "#app",
+	initialize: function() {
+		
+	},
+	
+	events: {
+	'click #have-swipes': 'swipeScreen',
+	'click #need-swipes': 'waitScreen',
+	},
+
+	swipeScreen: function() {
+		app.people = new app.PersonList();
+		app.people.fetch();
+		var personView = new app.PersonView({model: app.temp})
+		$("#main").html(personView.render().el)
+		//$("#main").html("alkfdjadsf")
+		while (!app.people.isEmpty()) {
+			$(this.el).html(people[0].render().el)
+		}  
+	},
+
+	waitScreen: function() {
+
+	}
+});
+
+app.appView = new appView();
+app.appView.swipeScreen();
