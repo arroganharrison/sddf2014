24d4dd01e8aa6eba18d9bd6d4439a3173648c24a
Harrison <harrison_tarr@yahoo.com>
2014-10-21 14:42:33 -0400

    Added function declarations

--- js/App.View.js
+++ js/App.View.js
@@ -1,5 +1,17 @@
 var appView = Backbone.View.extend({
+	el: "#app",
+	initialize: function() {
+		
+		this.
+	},
 
+	swipeScreen: function() {
+
+	},
+
+	waitScreen: function() {
+		
+	}
 });
 
 app.appView = new appView();
